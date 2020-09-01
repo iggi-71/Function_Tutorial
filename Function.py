4f57f096a7acb568a65cf27b71c5085fb968183f
iggi-71 <jimenez72iggi@gmail.com>
2020-09-01 12:40:22 -0700

    Functions

---
+++ Function.py
@@ -0,0 +1,29 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+result1 = area(10, 5)
+result2 = area(2, 8)
+print(result)
+print("")
+
+#subtract functiom
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+sum3 = subtract(1, 5)
+sum1 = subtract(10, 5)
+sum2 = subtract(4, 1)
+print(sum3)
+print(sum1)
+print(sum2)
+print("")
+
+#add function
+def add(add1, add2):
+    result = add1 + add2
+    return result
+adi = add(10, 5)
+adi1 = add(9, 9)
+print(adi)
+print(adi1)
